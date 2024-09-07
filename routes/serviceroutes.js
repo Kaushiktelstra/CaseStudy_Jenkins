e53074425e7d7fd9a7ce0fd8d579b8a0bd3841c8
Kaushik <MKaushik.Kaushik@team.telstra.com>
2024-09-07 17:27:13 +0530

    Commiting backend files for jenkins setup

---
+++ routes/serviceroutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { getAllServices,selectService, activateService } = require('../controllers/servicecontroller');
+const authMiddleware = require('../middleware/authMiddleware');
+ 
+const router = express.Router();
+ 
+router.post('/select-service',  selectService);
+router.post('/activate-service',  activateService);
+router.get('/get-services',getAllServices);
+ 
+module.exports = router;
